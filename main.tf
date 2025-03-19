5398e8b1cd4687a7b7cbb0aeb13922449ac4b9c8
rishikherde <rishikherde8@gmail.com>
2025-03-19 10:57:45 +0530

    change up

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 provider "azurerm" {
   features{}
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
   name = "test-resgrp-24072"
   location = "East US"
 }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "mytfstatesa"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
